e3433e12ba4aeeb025650af242f57844f51f5297
garaycarlosalfredo <68791092+garaycarlosalfredo@users.noreply.github.com>
2021-05-09 17:27:33 -0300

    init

--- back-end/main.py
+++ back-end/main.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 #Clase 68 - 101 pyhton 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/medical-shift2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+# Order matters: Initialize SQLAlchemy before Marshmallow
+ma = Marshmallow(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -19,67 +22,118 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-##CREATE TABLE
-class User(UserMixin, db.Model):
+##TABLE DATABASE
+class Appointment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime)
+    appointment_date = db.Column(db.DateTime)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    patient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    activity_1 = db.Column(db.String(100), db.ForeignKey('activity.id'))
+
+class Activity(db.Model):
+    id = db.Column(db.String(100), primary_key=True)
+    qantity = db.Column(db.Integer)
+
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-    rol = db.Column(db.Integer, db.ForeignKey('rol.id'),nullable=False)
+    rol_id = db.Column(db.String(100), db.ForeignKey('rol.id'))
+    rol = db.relationship("Rol", backref="users")
 
-    def to_dict(self):
-        #Method 1. 
-        dictionary = {}
-        # Loop through each column in the data record
-        for column in self.__table__.columns:
-            #Create a new dictionary entry;
-            # where the key is the name of the column
-            # and the value is the value of the column
-            dictionary[column.name] = getattr(self, column.name)
-        return dictionary
-        
-        #Method 2. Altenatively use Dictionary Comprehension to do the same thing.
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+    def __init__(self,email,password,name,rol_id):
+        self.email = email
+        self.password = password
+        self.name = name
+        self.rol_id = rol_id
 
-class Rol(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+
+class Rol(db.Model):
+    id = db.Column(db.String(100), primary_key=True)
     rol = db.Column(db.String(100), unique=True)
 
-    def to_dict(self):
-        #Method 1. 
-        dictionary = {}
-        # Loop through each column in the data record
-        for column in self.__table__.columns:
-            #Create a new dictionary entry;
-            # where the key is the name of the column
-            # and the value is the value of the column
-            dictionary[column.name] = getattr(self, column.name)
-        return dictionary
-        
-        #Method 2. Altenatively use Dictionary Comprehension to do the same thing.
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+##MARSHMALLOW SCHEMA 
+class UserSchema(ma.SQLAlchemySchema):
+    class Meta:
+        fields = ('id', 'email', 'password', 'name','rol_id')
 
-#db.create_all()
+class UserBasicSchema(ma.SQLAlchemySchema):
+    class Meta:
+        fields = ('id', 'email', 'name','rol_id')
 
+class RolSchema(ma.SQLAlchemySchema):
+    class Meta: 
+        fields = ('id','rol')
+
+class ActivitySchema(ma.SQLAlchemySchema):
+    class Meta:
+        field = ('id','qantity')
+
+class AppointmentSchema(ma.SQLAlchemySchema):
+    class Meta:
+        fields = ('id','date','appointment_date','doctor_id','patient_id','activity_1','activity_2','activity_3','activity_4')
+
+db.create_all()
+
+#marswmallow instances
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+userbasic_schema = UserBasicSchema()
+usersbasic_schema = UserBasicSchema(many=True)
+
+rol_schema = RolSchema()
+rols_schema = RolSchema(many=True)
+
+activity_schema = ActivitySchema()
+activities_schema = ActivitySchema(many=True)
+
+appointment_schema = AppointmentSchema()
+appointment_schema = AppointmentSchema(many = True)
 
 ##SOLO PARA PRUEBAS################################################
-@app.route('/usuarios', methods=["GET"])
+@app.route('/master_users', methods=["GET"])
 def get_usuarios():
-    usuarios = db.session.query(User).all()
-    return jsonify(usuarios=[usuario.to_dict() for usuario in usuarios])
+    usuarios = User.query.all()
+    result = users_schema.dump(usuarios)
+    return jsonify(result)
 
-@app.route('/roles', methods=["GET"])
+@app.route('/users', methods=["GET"])
+def get_usuariosbasico():
+    usuarios = User.query.all()
+    result = usersbasic_schema.dump(usuarios)
+    return jsonify(result)
+
+@app.route('/rols', methods=["GET"])
 def get_roles():
-    roles = db.session.query(Rol).all()
-    return jsonify(roles=[rol.to_dict() for rol in roles])
-    
+    roles = Rol.query.all()
+    result = rols_schema.dump(roles)
+    return jsonify(result)
+
+#POST
+@app.route('/adduser',methods=['POST'])
+def insert_user():
+    email = request.json['email']
+    password = request.json['password']
+    name = request.json['name']
+    rol_id = request.json['rol_id']
+
+
+    nuevo_usuario = User(email, password,name,rol_id)
+
+    db.session.add(nuevo_usuario)
+    db.session.commit()
+    return user_schema.jsonify(nuevo_usuario)
+
 ##SOLO PARA PRUEBAS################################################
 
 @app.route('/')
 def home():
     # Every render_template has a logged_in variable set.
-    return render_template("index.html", logged_in=current_user.is_authenticated)
-
+    #return render_template("index.html", logged_in=current_user.is_authenticated)
+    return "API-REST"
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
